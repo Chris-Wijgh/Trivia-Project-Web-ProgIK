e0d23a34088a4cd949b4f9969ab22709dbb119db
Chris Wijgh <chris@wijgh.nl>
2019-01-16 11:28:08 +0000

    task distribution

--- trivia/application.py
+++ trivia/application.py
@@ -24,6 +24,93 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+# front page
+@app.route("/")
+@app.route("/index", methods=["GET"])
+
+# front page linking to login and registration
+
+### TODO Jesper
+
+
+# login
+@app.route("/login", methods=["GET", "POST"])
+
+# login page for registered users
+
+### TODO Chris
+
+
+# register
+@app.route("/register", methods=["GET", "POST"])
+
+# registration + login for unregistered users
+
+### TODO Chris
+
+
+# index
+@app.route("/index", methods=["GET", "POST"])
+@L
+
+# user homepage and own stats
+
+### TODO Chris
+
+# questions
+@app.route("/questions", methods=["GET", "POST"])
+@L
+
+# user gets trivia questions to answer
+
+### TODO Dido
+
+# result
+@app.route("/result", methods=["GET"])
+@L
+
+# user gets right/wrong + correct answers, stores score stats in user-stats DB
+
+### TODO Dido
+
+# Top 10
+@app.route("/top10", methods=["GET"])
+@L
+
+# user gets top 10 lists of NR questions correct and % questions correct
+
+### TODO Chris
+
+# compare
+@app.route("/compare", methods=["GET", "POST"])
+@L
+
+# user can search for onther user's stats based on user name
+
+### TODO Chris
+
+
+# compared
+@app.route("/compared", methods=["GET", "POST"])
+@L
+
+# user gets stats based on query in compare + can search again
+
+### TODO Chris
+
+# apology
+@app.route("/apology", methods=["GET"])
+
+# page when something goes wrong
+
+### TODO Jesper
+
+
+
+
+
+
+
 
 
 
@@ -43,7 +130,7 @@ db = SQL("sqlite:///finance.db")
 @app.route("/index", methods=["GET", "POST"])
 @login_required
 def index():
-comment
+
 """
 
 
--- trivia/functions.py
+++ trivia/functions.py
@@ -5,3 +5,54 @@ from flask import redirect, render_template, request, session
 from functools import wraps
 
 
+def login():
+    # login function
+    """ TODO Chris """
+
+def register():
+    # registration and login function
+    """ TODO Chris """
+
+def pHash():
+    # encrypts user password
+    """ TODO Chris """
+
+def L():
+    # check if user is logged in
+    """ TODO Chris """
+
+def logout():
+    # logs user out
+    """ TODO Chris """
+
+def stats():
+    #  gives user NR correct and % correct
+    """ TODO Chris """
+
+def ranks():
+    # gives user ranking in NR and % lists
+    """ TODO Chris """
+
+def questions():
+    # gives list of lists of questions from external DB
+    """ TODO Dido """
+
+def store():
+    # stores data of answered question in user's stat DB
+    """ TODO Chris """
+
+def topNR():
+    # gives top 10 of users based on questions answered correctly
+    """ TODO Chris """
+
+def topP():
+    # gives top 10 of users based on % questions answered correctly
+    """ TODO Chris """
+
+def compare():
+    # search for onther user's stats based on user name
+    """ TODO Chris """
+
+def result():
+    # checks answers + provides correct answer
+    """ TODO Dido """
